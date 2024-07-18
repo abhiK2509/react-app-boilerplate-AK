dc24ffff0271e329539837664cc4e8354ddde10c
Abhijeet <kolay.akash@gmail.com>
2024-07-19 01:07:58 +0530

    Basic styling updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,22 +6,22 @@ const Header = () => {
     const onlineStatus = useOnlineStatus();
 
     return (
-        <div className="flex justify-between">
-            <div className="logo-container">
-                <img className="w-56" src={LOGO_URL} />
-            </div>
+        <div className="flex justify-between bg-pink-100 shadow-lg">
             <div>
-                <ul className="flex">
-                    <li>
+                <img className="w-24" src={LOGO_URL} />
+            </div>
+            <div className="flex items-center">
+                <ul className="flex p-4 m-4">
+                    <li className="px-4">
                         Online Status: {onlineStatus ? "✅" : "❎"}
                     </li>
-                    <li>
+                    <li className="px-4">
                         <Link to="/">Home</Link>
                     </li>
-                    <li>
+                    <li className="px-4">
                         <Link to="/about">About Us</Link>
                     </li>
-                    <li>
+                    <li className="px-4">
                         <Link to="/contact">Contact Us</Link>
                     </li>
                 </ul>
